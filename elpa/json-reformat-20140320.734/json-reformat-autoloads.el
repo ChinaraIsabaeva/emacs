f02a8074f50e6033743af97ea5e6a71f5b580fdf
chinara <chinara.isabaeva@gmail.com>
2014-12-07 23:45:10 +0000

    initial commit

---
+++ elpa/json-reformat-20140320.734/json-reformat-autoloads.el
@@ -0,0 +1,29 @@
+;;; json-reformat-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (json-reformat-region) "json-reformat" "json-reformat.el"
+;;;;;;  (21546 34126 710656 332000))
+;;; Generated autoloads from json-reformat.el
+
+(autoload 'json-reformat-region "json-reformat" "\
+Reformat the JSON in the region from BEGIN to END.
+
+\(fn BEGIN END)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("json-reformat-pkg.el") (21546 34126 782092
+;;;;;;  13000))
+
+;;;***
+
+(provide 'json-reformat-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; json-reformat-autoloads.el ends here
